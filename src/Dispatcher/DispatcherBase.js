b633adffd7da0a1bfd8e691a49f46ed0e2c3b592
mickey <mickey@yeeuu.com>
2015-09-13 10:55:51 +0800

    Complete DispatcherBase

--- src/Dispatcher/DispatcherBase.js
+++ src/Dispatcher/DispatcherBase.js
@@ -1,5 +1,34 @@
-//DistpatcherBase.js
+var DispatcherBase = function() {
+	this.registered = [] ;
+	this.prioritySet = new Set();
+};
 
-var DispatcherBase = function() {}
-
-DispathcerBase.prototype = {}
+DispathcerBase.prototype = {
+	//处理注册请求
+	register : function(registerRequest) {
+		var priority = registerRequest.priority || 0;
+		this.prioritySet.add(priority);
+		this.registered[priority] = this.registered[priority] || [];
+		this.registered[priority].push(registerRequest);	
+	},
+	dispatch : function(message) {
+		var prioritySetIterator = this.prioritySet[Symbol.iterator]();
+		this.dispatchWithPrority(message,prioritySetIterator);
+	},
+	dispatchWithPrority	: function(message,prioritySetIterator) {
+		var priority = this.prioritySetIterator.value;
+		if(!priority){
+			return true;
+		}
+		var registerRequests = this.registered[priority];
+		var promises = registerRequests.map(function(reg,i){
+			return Promise.resolve(reg.handler(message));
+		});
+		Promise.all(promises)
+		.then(function(){
+			return this.dispatchWithPrority(message,prioritySetIterator.next());
+		}.bind(this),function(err){
+			new Error("Error during dispatching");
+		});
+	}
+};
